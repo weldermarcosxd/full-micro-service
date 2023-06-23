18ec29f2942f4074552c11741d1174a2beed95b7
welder.silva <welder.silva@softplan.com.br>
2023-06-23 13:15:28 -0300

    hooks intro

--- app/v2/src/templates/Home/index.jsx
+++ app/v2/src/templates/Home/index.jsx
@@ -1,82 +1,77 @@
 import './styles.css';
 
-import { Component } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { loadPosts } from '../../utils/load-posts';
 import { Posts } from '../../components/Posts';
 import { Botao } from '../../components/Botao';
 import { CaixaDeTexto } from '../../components/CaixaDeTexto';
 
-class Home extends Component{
 
-  state = {
-    posts: [],
-    allPosts: [],
-    postsPerPage: 2,
-    currentPage: 0,
-    searchValue: ""
-  }
+export const Home = () => {
 
-  async componentDidMount(){
-    await this.loadPosts(); 
-  }
+  const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [postsPerPage] = useState(6);
+  const [allPosts, setAllPosts] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
-  loadPosts = async () => {
-    const { currentPage, postsPerPage } = this.state;
+  const noMorePosts = currentPage + postsPerPage >= allPosts.length;
+
+  const filteredPosts = !!searchValue ? allPosts.filter(post => {
+    return post.title.toLowerCase().includes(searchValue.toLocaleLowerCase())
+  }) : posts;
+
+  const handleLoadPosts = useCallback(async (currentPage, postsPerPage) => {
     const postsAndPhostos = await loadPosts();
-    this.setState({
-      posts:  postsAndPhostos.slice(currentPage * postsPerPage,postsPerPage),
-      allPosts:  postsAndPhostos 
-    });
-  }
 
-  loadMorePosts = () => {
-    const {currentPage, postsPerPage, allPosts, posts } = this.state;
+    setPosts(postsAndPhostos.slice(currentPage,postsPerPage))
+    setAllPosts(postsAndPhostos)
+  }, [])
+
+  useEffect(() => {
+    handleLoadPosts(0, postsPerPage);
+  }, [handleLoadPosts, postsPerPage]);
+
+  const loadMorePosts = () => {
     const nextPage = currentPage + postsPerPage;
     const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
     posts.push(...nextPosts);
-    this.setState({posts, currentPage: nextPage});
+
+    setPosts(posts);
+    setCurrentPage(nextPage);
   }
 
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const {value} = e.target;
-    this.setState({ searchValue: value });
+    setSearchValue(value);
   }
 
-  render() {
-    const { posts, currentPage, postsPerPage, allPosts, searchValue } = this.state;
-    const noMorePosts = currentPage + postsPerPage >= allPosts.length;
+  return (
+    <section className='container'>
+      <div className='search-container'> 
+        <CaixaDeTexto onChange={handleChange} value={searchValue}></CaixaDeTexto>
+      </div>
 
-    const filteredPosts = !!searchValue ? allPosts.filter(post => {
-      return post.title.toLowerCase().includes(searchValue.toLocaleLowerCase())
-    }) : posts;
-
-    return (
-      <section className='container'>
-        <div className='search-container'> 
-          <CaixaDeTexto onChange={this.handleChange} value={searchValue}></CaixaDeTexto>
-        </div>
-
-        {filteredPosts.length > 0 &&
-        (
-          <Posts posts={filteredPosts}></Posts>
-        )}
-          
-        {filteredPosts.length === 0 && (
-          <div>
-            <h3>Pesquisa não retornou valor</h3>
-            <Posts posts={posts}></Posts>
-          </div>
-        )}
+      {filteredPosts.length > 0 &&
+      (
+        <Posts posts={filteredPosts}></Posts>
+      )}
         
-        {!searchValue && (
-          <div className='button-container'>
-          <Botao text="Carregar mais posts" onClick={this.loadMorePosts} disabled={noMorePosts}></Botao>
+      {filteredPosts.length === 0 && (
+        <div>
+          <h3>Pesquisa não retornou valor</h3>
+          <Posts posts={posts}></Posts>
         </div>
-        )}
-        
-      </section>
-    );
-  }
+      )}
+      
+      {!searchValue && (
+        <div className='button-container'>
+        <Botao text="Carregar mais posts" onClick={loadMorePosts} disabled={noMorePosts}></Botao>
+      </div>
+      )}
+      
+    </section>
+  );
 }
 
 export default Home;
