8036d31a9b263a9d58ffe54473c3526f9ac93959
welder.silva <welder.silva@softplan.com.br>
2023-08-02 12:36:37 -0300

    cloudnary

--- app/v3/strapi-landing-pages/config/middlewares.js
+++ app/v3/strapi-landing-pages/config/middlewares.js
@@ -1,6 +1,25 @@
 module.exports = [
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
