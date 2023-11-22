42b0f22382bc85331c5170f3f154787051b2953b
Welder Marcos <welder.silva@gmail.com>
2023-11-22 08:07:27 -0300

    form edição

--- app/vnext-ui/components/ModalDeEdicaoDeProduto/index.tsx
+++ app/vnext-ui/components/ModalDeEdicaoDeProduto/index.tsx
@@ -1,9 +1,9 @@
-import { Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from "@nextui-org/react";
-import { Button } from "@nextui-org/react";
+import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Checkbox, Input, Link} from "@nextui-org/react";
 import useSWR, { mutate } from "swr";
 import { IProduto } from "./types/produtos";
 import { ExtensoesNumericas } from "@/utils/extensoes-numericas";
 import { ModalDeEdicaoProdutoProps } from "./types/modalDeEdicaoProps";
+
 
 const fetcher = (url: string) : Promise<IProduto> => fetch(url).then((res) => res.json());
 
@@ -14,6 +14,10 @@ export default function ModalDeEdicaoProduto(props: ModalDeEdicaoProdutoProps)
       onSuccess: () => console.log("chamada a produtos")
     });
 
+    function alterarValor(valor : string){
+      produtoResposta.preco = int.parse(valor);
+    }
+
     return (
         <Modal isOpen={props.disclosudeProps.isOpen} onOpenChange={props.disclosudeProps.onOpenChange} backdrop="blur">
         <ModalContent>
@@ -21,22 +25,51 @@ export default function ModalDeEdicaoProduto(props: ModalDeEdicaoProdutoProps)
             <>
               <ModalHeader className="flex flex-col gap-1">{produtoResposta?.nome}</ModalHeader>
               <ModalBody>
-                <p> 
-                    {isLoading ? "Carregando..." : "Id: " + produtoResposta?.id}
-                </p>
-                <p> 
-                    {isLoading ? "Carregando..." : "Sequencial: " + produtoResposta?.sequencial}
-                </p>
-                <p> 
-                    {isLoading ? "Carregando..." : "Descrição: " + produtoResposta?.descricao}
-                </p>
-                <p> 
-                    {isLoading ? "Carregando..." : "Preço: " + ExtensoesNumericas.formatarValorMonetario(produtoResposta!.preco)}
-                </p>
-                <p> 
-                    {isLoading ? "Carregando..." : "Quantidade Em Estoque: " + ExtensoesNumericas.formatarQuantidade(produtoResposta!.quantidadeEmEstoque)}
-                </p>
-
+              <Input
+                  autoFocus
+                  label="Id"
+                  placeholder="Id do produto"
+                  variant="bordered"
+                  isDisabled
+                  value={produtoResposta?.id}
+                />
+                <Input
+                  autoFocus
+                  label="Sequencial"
+                  placeholder="Sequencial do produto"
+                  variant="bordered"
+                  isDisabled
+                  value={produtoResposta?.sequencial?.toString()}
+                />
+                <Input
+                  autoFocus
+                  label="Nome"
+                  placeholder="Nome do produto"
+                  variant="bordered"
+                  value={produtoResposta?.nome.toString()}
+                  onValueChange={alterarValor}
+                />
+                <Input
+                  autoFocus
+                  label="Descrição"
+                  placeholder="Descrição do produto"
+                  variant="bordered"
+                  defaultValue={produtoResposta?.descricao?.toString()}
+                />
+                <Input
+                  autoFocus
+                  label="Valor Unitário"
+                  placeholder="0.0"
+                  variant="bordered"
+                  defaultValue={produtoResposta?.preco?.toString()}
+                />
+                <Input
+                  autoFocus
+                  label="Quantidade Em Estoque"
+                  placeholder="0.0"
+                  variant="bordered"
+                  defaultValue={produtoResposta?.quantidadeEmEstoque?.toString()}
+                />
               </ModalBody>
               <ModalFooter>
                 <Button onPress={onClose} color={props.cor}>
